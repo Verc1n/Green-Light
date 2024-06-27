4f60b8ca3782f195865b2ea2c5b9c32cc9e031b2
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-06-27 19:38:29 +1000

    Page nav
    Flyout navigation bar with the four pages

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,6 +5,11 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
+            Routing.RegisterRoute(nameof(SummaryPage), typeof(SummaryPage));
+            Routing.RegisterRoute(nameof(DatabsePage), typeof(DatabsePage));
         }
     }
 }
