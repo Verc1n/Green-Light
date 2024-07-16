af54a484145b6dae923e3c412b48f288ba7e3c17
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-16 18:52:47 +1000

    Building database for conditions

---
+++ Database Bits/MasterDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Green_Light.Models;
+
+namespace Green_Light.Database_Bits
+{
+    public class DriveConditionDatabase
+    {
+        SQLiteAsyncConnection Database;
+
+        public DriveConditionDatabase()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(DatabaseConstants.DatabasePath, DatabaseConstants.Flags);
+            var result = await Database.CreateTableAsync<DriveCondition>();
+        }
+
+        public AsyncTableQuery Task<>
+    }
+}
--- Models/DriveCondition.cs
+++ Models/DriveCondition.cs
@@ -1,7 +1,11 @@
-﻿namespace Green_Light.Models
+﻿using SQLite;
+
+namespace Green_Light.Models
 {
     public class DriveCondition
     {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
         public string strName { get; set; }
         public string strImageURL { get; set; }
         public string strCategory { get; set; }
