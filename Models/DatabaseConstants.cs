12b08a8da4238f6976cb6238b913ed5b20b635e3
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-16 11:10:10 +1000

    set up sqlite stuff

---
+++ Models/DatabaseConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Light.Models
+{
+    class DatabaseConstants
+    {
+        public const string strFileName = "DriveConditions.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, strFileName);
+    }
+}
