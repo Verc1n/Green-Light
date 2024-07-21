d1d86f87c7bc86a2375968a438aa2ef38c9df532
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-21 14:45:52 +1000

    added database stuff to drive and account classes

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Green_Light.Models
 {
     class Account
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string strName { get; set; }
         public int intPIN { get; set; }
         public bool bolIsSupervisor { get; set; }
--- Models/Drive.cs
+++ Models/Drive.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Green_Light.Models;
+using SQLite;
 
 namespace Green_Light.Models
 {
     class Drive
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public DriveCondition[] cdnDriveConditions { get; set; }
         public DateTime dtmDriveDateTime { get; set; }
         public TimeSpan tspDriveTime { get; set; }
