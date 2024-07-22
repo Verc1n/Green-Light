056782596cc9e8b9e03b8d5fa125ed6fcd264331
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-22 22:02:11 +1000

    timer bits working

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,27 +1,65 @@
-﻿namespace Green_Light
+﻿using System.Diagnostics;
+using System.Timers;
+
+namespace Green_Light
 {
     public partial class MainPage : ContentPage
     {
+        System.Timers.Timer tmrDriveTimer;
+        DateTime dtmStartTime;
+        TimeSpan tspPausedTime;
         public MainPage()
         {
             InitializeComponent();
+            tspPausedTime = TimeSpan.Zero;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            TimeSpan ElapsedTime = (DateTime.Now - dtmStartTime) + tspPausedTime;
+            MainThread.BeginInvokeOnMainThread(() =>{
+                lblTimer.Text=$"Elapsed Time: {ElapsedTime:h\\:mm\\:ss}";
+            });
+
         }
 
         private void OnStartButtonClicked(object sender, EventArgs e)
-        {
-            StartButton.IsVisible = false;
-            PauseButton.IsVisible = true;
-            StopButton.IsVisible = true;
+        {            
+            btnStart.IsVisible = false;
+            btnPause.IsVisible = true;
+            btnStop.IsVisible = true;
+            lblDrive.IsVisible= true;
+            lblRecordingIndicator.IsVisible= true;
+            lblTimer.IsVisible= true;
+
+            tmrDriveTimer = new System.Timers.Timer(1000);
+            tmrDriveTimer.Elapsed += OnTimedEvent;
+            dtmStartTime = DateTime.Now;
+            tmrDriveTimer.Start();
         }
 
         private void OnStopButtonClicked(object sender, EventArgs e)
         {
-
+            tmrDriveTimer.Enabled=false;
+            tmrDriveTimer.Dispose();
         }
 
         private void OnPauseButtonClicked(object sender, EventArgs e)
         {
+            switch (btnPause.Source.ToString())
+            {
+                case "File: pause.png":
+                    btnPause.Source = "play.png";
+                    lblRecordingIndicator.TextColor = Colors.Red;
+                    lblRecordingIndicator.Text = "Paused";
+                    break;
+                case "File: play.png":
+                    btnPause.Source = "pause.png";
+                    lblRecordingIndicator.TextColor = Colors.Green;
+                    lblRecordingIndicator.Text = "Recording";
+                    break;
 
+            }
         }
     }
 
