e0ddef04f9410906b08cdf11126278026523e083
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-22 11:43:29 +1000

    added pause/play/stop button function

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,6 +6,23 @@
         {
             InitializeComponent();
         }
+
+        private void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            StartButton.IsVisible = false;
+            PauseButton.IsVisible = true;
+            StopButton.IsVisible = true;
+        }
+
+        private void OnStopButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnPauseButtonClicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
