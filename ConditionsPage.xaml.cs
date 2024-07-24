724644a62983c780a240c232084677a5d84b54a4
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-07-24 13:01:07 +1000

    little bit of setup for conditions input

--- ConditionsPage.xaml.cs
+++ ConditionsPage.xaml.cs
@@ -7,11 +7,17 @@ namespace Green_Light;
 
 public partial class ConditionsPage : ContentPage
 {
+	Dictionary<DriveCondition, string> ConditionDriveBindings;
     public ConditionsPage(Drive drive)
 	{
 		MasterDatabase masterDatabase = new MasterDatabase();
 		InitializeComponent();
 		lblDriveTime.Text = string.Format("{0} hours {1} minutes", (int)drive.tspDriveTime.TotalHours, (int)drive.tspDriveTime.TotalMinutes);
         BindingContext = new DriveConditionsViewModel(masterDatabase);
+	}
+
+	private void btnSubmitClicked(object sender, EventArgs e)
+	{
+
 	}
 }
