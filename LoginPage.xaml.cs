6fe1c4c8b2ed1bc0c7c306e9262e1d738371016c
Verc1n <136546706+Verc1n@users.noreply.github.com>
2024-08-02 22:06:59 +1000

    minor login adjustments

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -5,21 +5,16 @@ namespace Green_Light;
 
 public partial class LoginPage : ContentPage
 {
-	Drive? drvUnfinishedDrive;
 
     public LoginPage(Drive drive)
 	{
-		InitializeComponent();
-		if (drive is not null) 
-		{
-            drvUnfinishedDrive = drive;
-        }		
+		InitializeComponent();		
         Routing.RegisterRoute(nameof(ConditionsPage), typeof(ConditionsPage));
     }
 
 
     public void btnLoginClicked(object sender, EventArgs e)
 	{
-		Navigation.PushAsync(new ConditionsPage(drvUnfinishedDrive));
+		
 	}
 }
